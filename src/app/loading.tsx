e9579df195fedffcfcc4b65d151c52f734bb0311
MrRobot <s.garazh2002@gmail.com>
2024-01-14 03:47:10 +0300

    v1.0.2

--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton"
-
-export default function LoadingHome() {
-	return (
-		<Skeleton className="w-[315px] h-[422px] rounded-full" />
-	);
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,14 @@ import { useEffect, useState } from 'react';
 import { getCurrentWeatherData } from '@/actions/getCurrentWeatherData';
 import CurrentWeather from '@/components/widgets/CurrentWeather';
 import { OpenWeatherData } from '@/lib/types';
+import { Loader2, NavigationOff } from 'lucide-react';
+import { Badge } from "@/components/ui/badge"
 
 export default function Home() {
   const [currentWeather, setCurrentWeather] = useState<OpenWeatherData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [Geolocation, setGeolocation] = useState<boolean>(false);
+  const savedCoordsString = localStorage.getItem('coords');
 
   const fetchData = async (latitude: number, longitude: number) => {
     try {
@@ -13,6 +18,7 @@ export default function Home() {
         lat: String(latitude),
         lon: String(longitude),
       });
+      setIsLoading(false);
       setCurrentWeather(currentWeatherDataRequest);
     } catch (error) {
       console.error('Error fetching data', error);
@@ -23,8 +29,11 @@ export default function Home() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
+          setGeolocation(true);
+          const { latitude, longitude } = position.coords;
+          const coordsString = `${latitude},${longitude}`;
+
+          localStorage.setItem('coords', coordsString);
           console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
           fetchData(latitude, longitude);
         },
@@ -37,8 +46,25 @@ export default function Home() {
     }
   }, []);
 
+  if (savedCoordsString) {
+    const [savedLatitude, savedLongitude] = savedCoordsString.split(',');
+    const latitude = parseFloat(savedLatitude);
+    const longitude = parseFloat(savedLongitude);
+
+    // Если координаты уже есть в localStorage, используем их для запроса данных
+    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    fetchData(latitude, longitude);
+  }
+
+
   return (
     <main className="grid place-items-center min-h-screen">
+      {isLoading && (
+        <Loader2 className='animate-spin w-[2.5rem] text-zinc-400' />
+      )}
+      {(!Geolocation && !savedCoordsString) && (
+        <Badge>Нет доступа к местоположению</Badge>
+      )}
       {currentWeather && (
         <CurrentWeather data={currentWeather} city={currentWeather.name} />
       )}
